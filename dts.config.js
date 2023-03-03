39f437e85eb43e63e1845b6542542639447c7fe1
Koeroesi86 <Koeroesi86@users.noreply.github.com>
2023-03-03 19:14:19 +0000

    deploy: e0aad93f06f35e6d1a14c6197ac166c41bf1f813

--- dts.config.js
+++
@@ -1,9 +0,0 @@
-/** @type import('dts-bundle-generator/config-schema').BundlerConfig */
-module.exports = {
-  entries: [
-    {
-      filePath: "./src/index.tsx",
-      outFile: "./dist/index.d.ts",
-    }
-  ]
-};
